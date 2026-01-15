165ce6318fec8f1200ebfbcb357f9dc85af1fc7d
Chamika Jayarathna <failgames.lk@gmail.com>
2026-01-15 13:19:31 +0530

    add inventory add, deduct, history, and search endpoints

--- app/Http/Controllers/InventoryController.php
+++ app/Http/Controllers/InventoryController.php
@@ -9,4 +9,12 @@ class InventoryController extends Controller
     public function index(){
 
     }
+
+    public function addItem(){}
+
+    public function deductItem(){}
+
+    public function history(){}
+
+    public function search(){}
 }
--- routes/web.php
+++ routes/web.php
@@ -16,5 +16,9 @@ Route::get('dashboard', function () {
 })->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::get('/inventory', [InventoryController::class, 'index'])->name('inventory.index');
+Route::post('/inventory/add', [InventoryController::class, 'addItem'])->name('inventory.add');
+Route::post('/inventory/deduct', [InventoryController::class, 'deductItem'])->name('inventory.deduct');
+Route::get('/inventory/{id}/history', [InventoryController::class, 'history'])->name('inventory.history');
+Route::get('/inventory/search', [InventoryController::class, 'search'])->name('inventory.search');
 
 require __DIR__.'/settings.php';
